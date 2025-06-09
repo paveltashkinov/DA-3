8f1e1ab4502d324c13af0f2386ab0495475f6643
paveltashkinov <pavel.tashkinov@nextmillennium.io>
2025-06-09 14:11:51 +0300

    Update Drop Tracker.sql

--- Hourly Revenue Spike/Drop Tracker.sql
+++ Hourly Revenue Spike/Drop Tracker.sql
@@ -10,39 +10,56 @@ Allow dynamic baseline resets (e.g., to account for advertiser changes).
 Highlight significant deviations (e.g., ±X% threshold).
 */
 
-with precalc as
+with td as
 (
     select site_id
-         , toDate(event_date) td
          , toHour(event_date) th
          , sum(sum_response_price)      AS revenue_response
          , sum(sum_response_price_pub)  AS revenue_pub_response
          , sum(sum_win_price)       AS revenue_wins
          , sum(sum_win_price_pub)       AS revenue_pub_wins    
       from header_bidder.statistic_group 
-     where event_date >= toDate(now() - INTERVAL 1 DAY)
+     where event_date > toDate(now())   
        and site_id = 11373 ---for example
   group by site_id
-         , toDate(event_date)
          , toHour(event_date)
-),
+), 
 
-unpv as
+yd as
 (
-    select site_id, td, th, 'revenue_response' metric, case when td = toDate(now()) then 1 else -1 end * revenue_response value
-      from precalc
- union all
-     select site_id, td, th, 'revenue_pub_response' metric, case when td = toDate(now()) then 1 else -1 end *revenue_pub_response value
-      from precalc
- union all
-     select site_id, td, th, 'revenue_wins' metric, case when td = toDate(now()) then 1 else -1 end *revenue_wins value
-      from precalc
- union all
-     select site_id, td, th, 'revenue_pub_wins' metric, case when td = toDate(now()) then 1 else -1 end *revenue_pub_wins value
-      from precalc
+    select site_id
+         , toHour(event_date) th
+         , sum(sum_response_price)      AS revenue_response
+         , sum(sum_response_price_pub)  AS revenue_pub_response
+         , sum(sum_win_price)       AS revenue_wins
+         , sum(sum_win_price_pub)       AS revenue_pub_wins    
+      from header_bidder.statistic_group 
+     where event_date <= toDate(now() - INTERVAL 1 DAY)
+       and event_date > toDate(now() - INTERVAL 2 DAY)
+       and site_id = 11373 ---for example 
+  group by site_id
+         , toHour(event_date)
 )
-    select site_id, td, th, metric, value
-         , sum(value)over(partition by site_id, th, metric) abs_difference
-         , case when abs(sum(value)over(partition by site_id, th, metric)) > 5 then 1 else 0 end is_highlighted_deviation
-      from unpv
-  order by metric, th, td desc 
+    select coalesce(td.site_id, yd.site_id) site_id
+         , coalesce(td.th, yd.th)  hr
+         
+         , td.revenue_response td_rev_response
+         , yd.revenue_response yd_rev_response
+         , 100 * abs(td.revenue_response - yd.revenue_response) / yd.revenue_response delta_rev_response
+         
+         , td.revenue_pub_response td_rev_pub_response
+         , yd.revenue_pub_response yd_rev_pub_response
+         , 100 * abs(td.revenue_pub_response - yd.revenue_pub_response) / yd.revenue_pub_response delta_rev_pub_response
+         
+         , td.revenue_wins td_rev_wins
+         , yd.revenue_wins yd_rev_wins
+         , 100 * abs(td.revenue_wins - yd.revenue_wins) / yd.revenue_wins delta_rev_wins
+         
+         , td.revenue_pub_wins td_rev_pub_wins
+         , yd.revenue_pub_wins yd_rev_pub_wins
+         , 100 * abs(td.revenue_pub_wins - yd.revenue_pub_wins) / yd.revenue_pub_wins delta_rev_pub_wins
+      from td
+ full join yd on td.site_id = yd.site_id 
+             and td.th = yd.th
+     where td.th<>0 ---to exclude hours that we yet have no data for
+  order by hr
